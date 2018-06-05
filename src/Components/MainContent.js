a7f545cd64547337aec84ab6edea2bd928e51c7b
Nick Nedev <nedevn@gmail.com>
2018-06-05 12:23:43 -0700

    Adds community reach image

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -34,12 +34,19 @@ class Navbar extends Component {
           </Row>
           <Row className="show-grid">
             <Col xs={12}>
-              <h2>A quote from the video</h2>
+              <blockquote>We reduce the harm to the community but also try to restore the deficits in the offenders life. </blockquote>
             </Col>
           </Row>
           <Row className="show-grid">
-            <Col xs={12}>
-              <h1>Meet the team.</h1>
+            <Col xs={12} sm={6}>
+              <h2>Community services</h2>
+              <Image responsive
+                src="https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/18424055_1435990569754998_4450440117694340328_n.png?_nc_cat=0&oh=ba69af112b2fda2e69ba2baa054629b4&oe=5BBC7538"
+                alt="Prosecutors Team"
+              />
+            </Col>
+            <Col xs={12} sm={6}>
+              <h2>Join our team!</h2>
               <Image responsive
                 src="https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/12657790_1049695061717886_8986899882012056349_o.jpg?_nc_cat=0&oh=b0d6a245bfb33edb4f02a958cf7d5e2d&oe=5B7D92C2"
                 alt="Prosecutors Team"
